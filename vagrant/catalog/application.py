2d0fa098e7d38280e304406f6a76585ad4b68457
PingSharp <dotping@online.de>
2019-03-14 09:26:17 +0100

    feat: add implementation to edit and delete item

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -18,11 +18,13 @@ def home():
     output = "<h1>Hi,Welcome</h1>"
     output += "<ol>"
     for c in cates:
-        output += "<li>%s</li>"%c.name
+        output += "<li><a href='http://localhost:9000/catalog/%s/items'>%s</a></li>"%(c.name,c.name)
     output += "</ol>"
+    output += "<a href='http://localhost:9000/catalog/add'>Add Item</a>"
     output += "<ol>"
     for i in items:
-        output += "<li>%s</li>"%i.name
+        cate = dc.getCategotyById(i.Category_id)
+        output += "<li><a href='http://localhost:9000/catalog/%s/%s'>%s</a></li>"%(cate.name,i.name,i.name)
     output += "</ol>"
     return output
 @app.route('/catalog/<catalog>/items')
@@ -41,37 +43,55 @@ def description(catalog,item):
     des = dc.getItemDescriptionByName(catalog,item)
     output = "<p>%s %s description here</p>"%(catalog,item)
     output += "<p>%s</p>"%des
+    output += "<a href='http://localhost:9000/catalog/%s/edit'>edit</a><br>"%item
+    output += "<a href='http://localhost:9000/catalog/%s/delete'>delete</a>"%item
     return output
-@app.route('/catalog/<item>/edit',methods=['GET','PUT'])
+@app.route('/catalog/<item>/edit',methods=['GET','POST'])
 def edit(item):
+    thisItem = dc.getItemByItemName(item)
+    cates = dc.getAllCategories()
+    cateId = dc.getCategotyById(thisItem.Category_id).id
     if request.method == 'GET':
-        output = "<h1>Item edit</h1>"
-        output += "<form action='http://localhost:9000/catalog/%s/edit' method='PUT'>"%item
-        output += "<input name='name' type='text'>"
-        output += "<input name='description' type='text'>"
+        output = "<h1>%s Item edit</h1>"%thisItem.name
+        output += "<form  method='POST'>"
+        output += "<input name='name' type='text' value='%s'>"%thisItem.name
+        output += "<input name='description' type='text' value='%s'>"%thisItem.description
+        output += "<select name='catalog'>"
+        for c in cates:
+            if c.id == cateId:
+                output += "<option value='%s' selected>%s</option>"%(c.name,c.name)
+            output += "<option value='%s'>%s</option>"%(c.name,c.name)
+        output += "</select>"
         output += "<input type='submit' value='Edit'>"
         output += "</form>"
         return output
-    elif request.method == 'PUT':
-        return None    
+    elif request.method == 'POST':
+        iName = request.form['name']
+        iDes = request.form['description'] 
+        iCate = request.form['catalog']
+        dc.editItem(iName,iDes,iCate,item)
+        return "<p>edit %s,%s,%s succesfully!</p>"%(iName,iDes,iCate)
 
-@app.route('/catalog/<item>/delete',methods=['GET','DELETE'])
+@app.route('/catalog/<item>/delete',methods=['GET','POST'])
 def delete(item):
+    thisItem = dc.getItemByItemName(item)
     if request.method == 'GET':
         output = "<h1>Item delete</h1>"
-        output += "<form action='http://localhost:9000/catalog/%s/delete' method='DELETE'>"%item
-        output += "<label>Are you sure you want to delete this items?</label>"
+        output += "<form action='http://localhost:9000/catalog/%s/delete' method='POST'>"%item
+        output += "<label>Are you sure you want to delete this item(%s)?</label>"%thisItem.name
         output += "<input type='submit' value='Delete'>"
         output += "</form>"
         return output
-    elif request.method == 'DELETE':
-        return None  
-@app.route('/catalog/<catalog>/add',methods=['GET','POST'])
-def add(catalog):
+    elif request.method == 'POST':
+        dc.deleteItem(thisItem)
+        return "<p>delete item succesfully!</p>"
+
+@app.route('/catalog/add',methods=['GET','POST'])
+def add():
     cates = dc.getAllCategories()
     if request.method == 'GET':
         output = "<h1>Add Item </h1>"
-        output += "<form action='http://localhost:9000/catalog/%s/add' method='POST'>"%catalog
+        output += "<form action='http://localhost:9000/catalog/add' method='POST'>"
         output += "<input type='text' name='name'><br>"
         output += "<input type='text' name='description'><br>"
         output += "<select name='catalog'>"
@@ -95,5 +115,5 @@ def json():
     return output
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
+    app.debug = False
     app.run(host='127.0.0.1',port = 9000)
--- vagrant/catalog/databaseController.py
+++ vagrant/catalog/databaseController.py
@@ -36,19 +36,37 @@ def getItemsByCatId(cId):
 
 #get Category Id from Category bane
 def getCategoriesIdByName(cName):
-    cid = session.query(Categories).filter_by(name = cName).one().id
+    cid = session.query(Categories).filter_by(name = cName).first().id
     return cid
 def getCategoryByName(cName):
-    category = session.query(Categories).filter_by(name=cName).one()
+    category = session.query(Categories).filter_by(name=cName).first()
+    return category
+def getCategotyById(cId):
+    category = session.query(Categories).filter_by(id = cId).first()
     return category
 #get Item description from item name and category name
 def getItemDescriptionByName(cName,iName):
     cid = getCategoriesIdByName(cName)
     itemdes = session.query(Item).filter_by(name = iName).filter_by(Category_id = cid).first().description 
     return itemdes
-
+#get Item object from item name
+def getItemByItemName(iName):
+    item = session.query(Item).filter_by(name = iName).first()
+    return item
+#add new Item object to the database
 def addNewItem(iName,iDes,categoryName):
     cate = getCategoryByName(categoryName)
     item = Item(name=iName,description=iDes,categories=cate)
     session.add(item)
+    session.commit()
+#edit item and commit the changes to the database
+def editItem(iName,iDes,iCate,thisItem):
+    oldItem = getItemByItemName(thisItem)
+    oldItem.name = iName
+    oldItem.description = iDes
+    oldItem.categories = getCategoryByName(iCate)
+    session.commit()
+#delete item from the database
+def deleteItem(dItem):
+    session.delete(dItem)
     session.commit()
