73bddb4a53d887c29716db0ea480eefa5798fb5f
PingSharp <dotping@online.de>
2019-03-15 16:47:21 +0100

    feat: add html  and style for add,edit,delete pages

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -30,85 +30,51 @@ def home():
     return render_template("home.html",categories=cates,listItems = items)
 @app.route('/catalog/<catalog>/items')
 def showItems(catalog):
+    cates = dc.getAllCategories()
     cid = dc.getCategoriesIdByName(catalog)
     items = dc.getItemsByCatId(cid)
-    output ="<p>%s items here</p>"%catalog
-    output += "<ol>"
-    for i in items:
-        output += "<li>%s</li>"%i.name
-    output += "</ol>"
-    return output
+    return render_template("home.html",categories=cates,listItems = items,cata=catalog)
 
 @app.route('/catalog/<catalog>/<item>')
 def description(catalog,item):
     des = dc.getItemDescriptionByName(catalog,item)
-    output = "<p>%s %s description here</p>"%(catalog,item)
-    output += "<p>%s</p>"%des
-    output += "<a href='http://localhost:9000/catalog/%s/edit'>edit</a><br>"%item
-    output += "<a href='http://localhost:9000/catalog/%s/delete'>delete</a>"%item
-    return output
+    return render_template("home.html",description = des,return_item = item)
 @app.route('/catalog/<item>/edit',methods=['GET','POST'])
 def edit(item):
     thisItem = dc.getItemByItemName(item)
     cates = dc.getAllCategories()
     cateId = dc.getCategotyById(thisItem.Category_id).id
     if request.method == 'GET':
-        output = "<h1>%s Item edit</h1>"%thisItem.name
-        output += "<form  method='POST'>"
-        output += "<input name='name' type='text' value='%s'>"%thisItem.name
-        output += "<input name='description' type='text' value='%s'>"%thisItem.description
-        output += "<select name='catalog'>"
-        for c in cates:
-            if c.id == cateId:
-                output += "<option value='%s' selected>%s</option>"%(c.name,c.name)
-            output += "<option value='%s'>%s</option>"%(c.name,c.name)
-        output += "</select>"
-        output += "<input type='submit' value='Edit'>"
-        output += "</form>"
-        return output
+        return render_template("edit.html",thisitem=thisItem,categories=cates,cId=cateId)
     elif request.method == 'POST':
         iName = request.form['name']
         iDes = request.form['description'] 
         iCate = request.form['catalog']
         dc.editItem(iName,iDes,iCate,item)
-        return "<p>edit %s,%s,%s succesfully!</p>"%(iName,iDes,iCate)
+        flash("You have edited this item succesfully!")
+        return redirect(url_for("home"))
 
 @app.route('/catalog/<item>/delete',methods=['GET','POST'])
 def delete(item):
     thisItem = dc.getItemByItemName(item)
     if request.method == 'GET':
-        output = "<h1>Item delete</h1>"
-        output += "<form action='http://localhost:9000/catalog/%s/delete' method='POST'>"%item
-        output += "<label>Are you sure you want to delete this item(%s)?</label>"%thisItem.name
-        output += "<input type='submit' value='Delete'>"
-        output += "</form>"
-        return output
+        return render_template("delete.html",thisitem=thisItem)
     elif request.method == 'POST':
         dc.deleteItem(thisItem)
-        return "<p>delete item succesfully!</p>"
-
+        flash("You have deleted this item succesfully!")
+        return redirect(url_for("home"))
 @app.route('/catalog/add',methods=['GET','POST'])
 def add():
     cates = dc.getAllCategories()
     if request.method == 'GET':
-        output = "<h1>Add Item </h1>"
-        output += "<form action='http://localhost:9000/catalog/add' method='POST'>"
-        output += "<input type='text' name='name'><br>"
-        output += "<input type='text' name='description'><br>"
-        output += "<select name='catalog'>"
-        output += "<option value=''>please choose a category</option>"
-        for c in cates:
-            output += "<option value='%s'>%s</option>"%(c.name,c.name)
-        output += "</select>"
-        output += "<input type='submit' value='Add'>"
-        output += "</form>"
-        return output
+        return render_template("add.html",categories=cates)
     elif request.method == 'POST':
         iName = request.form['name']
         iDes = request.form['description']
         iCate = request.form['catalog']
         dc.addNewItem(iName,iDes,iCate)
-        return "<p>add %s,%s,%s succesfully!</p>"%(iName,iDes,iCate)
+        flash("You have added the new item succesfully!")
+        return redirect(url_for("home"))
      
 @app.route('/catalog.json')
 def json():
